a3e118f3fa707577260f861945771c72562f0af1
KlasClaywood <klas.claywood@gmail.com>
2016-10-24 15:35:26 +0200

    First draft of objects

---
+++ Console-BloodBowlManager/Objects/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_BloodBowlManager.Objects
+{
+    class Player
+    {
+        public Positional Position { get; private set; }
+        public string Name { get; private set; }
+        public int Jersey { get; private set; }
+        public int SPP { get; set; }
+        public int MA { get; set; }
+        public int ST { get; set; }
+        public int AG { get; set; }
+        public int AV { get; set; }
+        public int Cost { get; set; }
+        public string[] Skills { set; get; }
+
+        public Player(string name, Positional position, int jersey)
+        {
+            Position = position;
+            Name = name;
+            Jersey = jersey;
+            MA = position.MA;
+            ST = position.ST;
+            AG = position.AG;
+            AV = position.AV;
+            SPP = 0;
+            Cost = position.Cost;
+            if (position.Skills != null)
+            {
+                List<string> skilllista = new List<string>();
+                foreach (string skill in position.Skills)
+                {
+                    skilllista.Add(skill);
+                }
+                Skills = skilllista.ToArray();
+            }
+            else Skills = new string[0];
+        }
+        public string ToString()
+        {
+            return Name + " the " + Position.Race + " " + Position.Name;
+        }
+    }
+}
---
+++ Console-BloodBowlManager/Objects/Positional.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_BloodBowlManager.Objects
+{
+    class Positional
+    {
+        public string Name { get; private set; }
+        public string Race { get; private set; }
+        public string[] Skills { get; private set; }
+        public int Cost { get; private set; }
+        public int ST { get; private set; }
+        public int AG { get; private set; }
+        public int MA { get; private set; }
+        public int AV { get; private set; }
+        public string[] NormalRoll { get; private set; }
+        public string[] DoubleRoll { get; private set; }
+
+        public Positional (string name, string race, string[] skills, int cost, int ma, int st, int ag, int av, string[] normals, string[] doubles)
+        {
+            Name = name;
+            Race = race;
+            Skills = skills;
+            Cost = cost;
+            MA = ma;
+            ST = st;
+            AG = ag;
+            AV = av;
+            NormalRoll = normals;
+            DoubleRoll = doubles;
+        }
+    }
+}
